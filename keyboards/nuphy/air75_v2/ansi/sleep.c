64e1839851bb656610a9bea93c8af57b2ea6a3f4
Your Name <you@me.him>
2024-02-23 12:44:08 +0200

    copyright

--- keyboards/nuphy/air75_v2/ansi/sleep.c
+++ keyboards/nuphy/air75_v2/ansi/sleep.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 @ Nuphy <https://nuphy.com/>
+Copyright 2023 @ Nuphy <https://nuphy.com/> & adi4086
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
