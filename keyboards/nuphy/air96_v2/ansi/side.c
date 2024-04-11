859d13aedadfb360165f1e514ef92e9399e19836
adi4086 <not.valid@email.com>
2024-04-11 23:13:17 +0300

    skip side led when sleep

--- keyboards/nuphy/air96_v2/ansi/side.c
+++ keyboards/nuphy/air96_v2/ansi/side.c
@@ -92,7 +92,6 @@ bool is_side_rgb_off(void)
 }
 
 void side_rgb_refresh(void) {
-    if (f_wakeup_prepare) return;
     if (!is_side_rgb_off() || user_config.ee_side_light > 0) {
         pwr_led_on(); // power on side LED before refresh
     }
@@ -663,6 +662,7 @@ void rgb_test_show(void) {
  * @brief  side_led_show.
  */
 void side_led_show(void) {
+    if (f_wakeup_prepare) return;
     static uint32_t side_update_time  = 0;
 
     // side_mode & side_speed should always be valid...
